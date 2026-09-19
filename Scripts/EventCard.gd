2f675cfc0a2b4bf578747774a25f2e587151d743
Jason Reimer <jasonreimer55@gmail.com>
2026-09-19 13:21:28 -0600

    Event Manager Baseline

---
+++ Scripts/EventCard.gd
@@ -0,0 +1,11 @@
+class_name EventCard
+extends Resource
+
+@export var id: String 
+@export var title: String 
+@export_multiline var description: String
+@export var requirements: String 
+@export var difficulty: int 
+@export var outcomes: Array[String] 
+
+ 
