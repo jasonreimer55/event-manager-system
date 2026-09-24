520c12d3ffcc9e03b483ac37b12ccd9178c1ef7d
Jason Reimer <jasonreimer55@gmail.com>
2026-09-23 21:27:28 -0600

    Cards Have Value

--- Scripts/EventCard.gd
+++ Scripts/EventCard.gd
@@ -4,8 +4,9 @@ extends Resource
 @export var id: String 
 @export var title: String 
 @export_multiline var description: String
-@export var requirements: String 
-@export var difficulty: int 
-@export var outcomes: Array[String] 
+@export var value: int
+#@export var requirements: String 
+#@export var difficulty: int 
+#@export var outcomes: Array[String] 
 
  
